594a669b16bbca3cdc4ea879a5e69d7af05b9ad0
Philip Welin-Berger <philip.welin.berger@gmail.com>
2022-09-16 15:41:24 +0200

    added modulo operation and global DOM variables

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const DISPLAY_VALUES = {
+  value1: '',
+  value2: '',
+  operator: '',
+};
+
+//Global DOM elements
+const NUMBER_BUTTONS = [...document.getElementsByClassName('number')];
+const CALCULATOR_DISPLAY = document.querySelector('calculator-display');
+if (!CALCULATOR_DISPLAY) throw new Error('Could not find calculator display!');
+
 const add = (x, y) => x + y;
 
 const subract = (x, y) => x - y;
@@ -5,15 +16,18 @@ const subract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 
 const divide = (x, y) => {
-  if (y === 0) return null;
+  if (y === 0) return 'Dividing by zero? Cringe!';
   return x / y;
 };
+
+const modulo = (x, y) => x % y;
 
 const OPERATION_OBJ = {
   '+': add,
   '-': subract,
   '*': multiply,
   '/': divide,
+  '%': modulo,
 };
 
 const operate = (operation, x, y) => {

924aa06208cc5b46af8f39ca45a2ab89160d16f6
Philip Welin-Berger <philip.welin.berger@gmail.com>
2022-09-16 16:20:01 +0200

    added AC functionality

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const DISPLAY_VALUES = {
 //Global DOM elements
 const NUMBER_BUTTONS = [...document.getElementsByClassName('number')];
 const OPERATION_BUTTONS = [...document.getElementsByClassName('operation')];
+const AC_BUTTON = document.getElementById('ac');
+const C_BUTTON = document.getElementById('c');
+const EQUAL_BUTTON = document.getElementById('equal');
+const DOT_BUTTON = document.getElementById('dot');
 const CALCULATOR_DISPLAY = document.querySelector('.calculator-display');
 if (!CALCULATOR_DISPLAY) throw new Error('Could not find calculator display!');
 
@@ -35,11 +39,8 @@ const operate = (operation, x, y) => {
   return OPERATION_OBJ[operation](x, y);
 };
 
-const numberClick = (e) => {
-  if (!DISPLAY_VALUES.operator) {
-    DISPLAY_VALUES.value1 += e.target.value;
-  }
-};
+const setCalculatorDisplayValue = () =>
+  (CALCULATOR_DISPLAY.textContent = `${DISPLAY_VALUES.value1} ${DISPLAY_VALUES.operator} ${DISPLAY_VALUES.value2}`);
 
 // Event listeners
 NUMBER_BUTTONS.forEach((n) => {
@@ -47,7 +48,7 @@ NUMBER_BUTTONS.forEach((n) => {
     let value = DISPLAY_VALUES.operator ? 'value2' : 'value1';
     DISPLAY_VALUES[value] += n.textContent;
     console.log('display value is: ', DISPLAY_VALUES[value]);
-    CALCULATOR_DISPLAY.textContent = `${DISPLAY_VALUES.value1} ${DISPLAY_VALUES.operator} ${DISPLAY_VALUES.value2}`;
+    setCalculatorDisplayValue();
     console.log('does this register?');
   });
 });
@@ -56,6 +57,13 @@ OPERATION_BUTTONS.forEach((o) => {
   o.addEventListener('click', () => {
     if (!DISPLAY_VALUES.value1) return;
     DISPLAY_VALUES.operator = o.textContent;
-    CALCULATOR_DISPLAY.textContent = `${DISPLAY_VALUES.value1} ${DISPLAY_VALUES.operator} ${DISPLAY_VALUES.value2}`;
+    setCalculatorDisplayValue();
   });
+});
+
+AC_BUTTON.addEventListener('click', () => {
+  DISPLAY_VALUES.value1 = '';
+  DISPLAY_VALUES.value2 = '';
+  DISPLAY_VALUES.operator = '';
+  setCalculatorDisplayValue();
 });
